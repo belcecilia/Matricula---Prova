eec9e064726694fa78f70489223a6baf85d2a0c7
ISABEL CECILIA GOMES ALCONSO <021.900250@senaicimatec.edu.br>
2024-08-30 17:59:01 -0300

    vitor passou aqui!

--- Matricula/Models/Students.cs
+++ Matricula/Models/Students.cs
@@ -6,8 +6,8 @@
         public int  CPF { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
-       // public string Curso { get; set; }   // Nome do curso
-        //public string Turno { get; set; }       // Turno
+        public string Curso { get; set; }   // Nome do curso
+        public string Turno { get; set; }       // Turno
         public DateTime DataNas { get; set; }  // Data de nascimento
     }
 }
---
+++ Matricula/Pages/alunos.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Matricula.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MySqlX.XDevAPI;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+public class alunosModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+    public alunosModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IList<Student> Students { get; set; }
+
+    public async Task OnGetAsync()
+    {
+        Students = await _context.Students.ToListAsync();
+
+    }
+    public async Task<IActionResult> OnPostAddAsync(Student newStudent)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        _context.Students.Add(newStudent);
+        await _context.SaveChangesAsync();
+        return RedirectToPage();
+    }
+
+    public async Task<IActionResult> OnPostDeleteAsync(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        if (student != null)
+        {
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToPage();
+    }
+
+
+}
